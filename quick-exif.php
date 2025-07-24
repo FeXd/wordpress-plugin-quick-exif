7bb830f918d4f58c6e487e25ca0bdf6d7d851d84
Arlin Schaffel <arlin@fexd.com>
2025-07-24 10:42:27 -0600

    Create Plugin File Created

---
+++ quick-exif.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Quick Exif
+ * Plugin URI: https://github.com/FeXd/wordpress-plugin-quick-exif
+ * Description: A simple WordPress plugin to help facilitate posting to my photography blog at https://arlin.photography.
+ * Version: 0.0.1
+ * Author URI: https://github.com/FeXd
+ * License: MIT
+ * License URI: https://github.com/FeXd/wordpress-plugin-quick-exif/blob/main/LICENSE
+ * Text Domain: quick-exif
+*/
+
